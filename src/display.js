54886de3c1f8264f88c37e5938ed8753be170e42
kylestech95 <kylesTech95@outlook.com>
2023-10-15 14:09:45 -0400

    updated click handlers

--- src/display.js
+++ src/display.js
@@ -8,39 +8,26 @@ export default class Display extends React.Component {
         super(props);
         this.state={
             session_min:25,
-            break_min:5
+            break_min:5,
+            session_sec:'00',
+            break_sec:'00'
         }
-
-    }
-    handleUp = () => {
-        this.setState({
-            sesssion_min: this.state.session_min <60 ? (this.state.session_min + 1) : this.state.session_min,
-            break_min: this.state.break_min <60 ? (this.state.break_min + 1) : this.state.break_min
-        })
-    }
-    
-    handleDwn = () => {
-        this.setState({
-            session_min: this.state.session_min > 1 ? (this.state.session_min - 1) : this.state.session_min,
-            break_min: this.state.break_min > 1 ? (this.state.break_min - 1) : this.state.break_min
-
-        })
     }
     
     render(){
         return (
            <>
         <div className="control-container">
-          <Break up={this.handleUp} dwn={this.handleDwn} minutes={this.state.session_min}/>
-          <Session up={this.handleUp} dwn={this.handleDwn}minutes={this.state.break_min}/>
+          <Break minutes={this.state.break_min} seconds={this.state.break_sec}/>
+          <Session minutes={this.state.session_min} seconds={this.state.session_sec}/>
         </div>
            {/*Display container*/}
         <div className="display-container">
             <h2 className="label">Session</h2>
             <div className="time-container">
-                <h1 className="minutes">00</h1>
+                <h1 className="minutes">{this.state.session_min}</h1>
                 <h1 className="colon">:</h1>
-                <h1 className="seconds">00</h1>
+                <h1 className="seconds">{this.state.session_sec}</h1>
             </div>
         </div>
            </> 
--- src/session.js
+++ src/session.js
@@ -5,10 +5,20 @@ export default class Session extends React.Component {
     constructor(props){
         super(props);
         this.state={
-            minutes:this.props.minutes
+            session_min:this.props.minutes,
+            session_sec:this.props.seconds    
         }
     }
-   
+   handleUp = () => {
+    this.setState({
+        session_min: this.state.session_min < 60 ? this.state.session_min + 1 : this.state.session_min
+    })
+   }
+   handleDown = () => {
+    this.setState({
+        session_min: this.state.session_min > 1 ? this.state.session_min - 1 : this.state.session_min
+    })
+   }
 
     render(){
         return (
@@ -21,9 +31,9 @@ export default class Session extends React.Component {
             </div>
             {/*session corols*/}
             <div className="session-controls">
-                <i id="session-up" className="fa-solid fa-arrow-up" ></i>{/*up arrow*/}
-                <p className={'session-num'}>{this.state.minutes}</p>{/*input*/}
-                <i id="session-down" className="fa-solid fa-arrow-down" ></i>{/*down arrow*/}
+                <i id="session-up" className="fa-solid fa-arrow-up" onClick={this.handleUp}></i>{/*up arrow*/}
+                <p className={'session-num'}>{this.state.session_min}</p>{/*input*/}
+                <i id="session-down" className="fa-solid fa-arrow-down" onClick={this.handleDown}></i>{/*down arrow*/}
             </div>
            </div>
            </> 
