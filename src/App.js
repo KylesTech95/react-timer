f0f91bf0bcb5148d715ae732924c2447052449df
kylestech95 <kylestech95@penguin>
2023-10-24 16:41:42 -0400

    timer is working but not 100%

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import React, { useEffect,useState } from 'react';
-import { formatMinutes,formatSeconds } from './helpers.js';
+import { formatMinutes,formatSeconds,moreThan10 } from './helpers.js';
 import { type } from '@testing-library/user-event/dist/type';
+
 class Break extends React.Component{
   constructor(props){
     super(props)
@@ -50,7 +51,8 @@ class Session extends React.Component{
 constructor(props){
   super(props);
   this.state = {
-    time:this.props.time,
+    s_time:this.props.s_time,
+    b_time:this.props.b_time,
     count: 0,
     min:this.props.min,
     max:this.props.max
@@ -64,26 +66,31 @@ render(){
       </div>
       <div className="session-controls">
         <span onClick={()=>{
-          var timeLeft = document.querySelector('#time-left')
+          var mins = document.querySelector('#time-left>h3.display-mins')
+          var secs = document.querySelector('#time-left>h3.display-secs')
           this.setState({
             count:this.state.count = 1
           })
-          if(this.state.time[0] > 1){
-            this.state.time[0] -= this.state.count
-            timeLeft.textContent = this.state.time[0]+":"+formatSeconds(this.state.time[1])
+          if(this.state.s_time[0] > 1){
+            this.state.s_time[0] -= this.state.count
+            mins.textContent = this.state.s_time[0]
+            secs.textContext = formatSeconds(this.state.s_time[1])
           }
 
         }} id="session-increment" className='material-symbols-outlined material'>arrow_forward_ios</span>{/*Up arrow*/}
-        <span id="session-length">{this.state.time[0]}</span>
+        <span id="session-length">{this.state.s_time[0]}</span>
         <span onClick={()=>{
-          var timeLeft = document.querySelector('#time-left')
-          this.setState({
-            count:this.state.count = 1
-          })
-          if(this.state.time[0] < 60){
-            this.state.time[0] += this.state.count
-            timeLeft.textContent = this.state.time[0]+":"+formatSeconds(this.state.time[1])
-          }
+         var mins = document.querySelector('#time-left>h3.display-mins')
+         var secs = document.querySelector('#time-left>h3.display-secs')
+         this.setState({
+           count:this.state.count = 1
+         })
+         if(this.state.s_time[0] > 0){
+           this.state.s_time[0] += this.state.count
+           mins.textContent = this.state.s_time[0]
+           secs.textContext = formatSeconds(this.state.s_time[1])
+         }
+
         }} id="session-decrement"className='material-symbols-outlined material'>arrow_back_ios</span>{/*Down arrow*/}
       </div>
     </div>
@@ -94,8 +101,8 @@ class Timer extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      time:this.props.time,
-      time2:this.props.time2,
+      s_time:this.props.s_time,
+      b_time:this.props.b_time,
       min:this.props.min,
       max:this.props.max
     }
@@ -108,7 +115,9 @@ class Timer extends React.Component{
         </div>
         <div className="time-left-container">
             <div id="time-left">
-              {this.state.time}
+              <h3 className="display-mins">{this.state.s_time[0]}</h3>
+              <h3 className="display-colon">:</h3>
+              <h3 className="display-secs ">{formatSeconds(this.state.s_time[1])}</h3>
             </div>
         </div>
       </div>
@@ -118,13 +127,75 @@ class Timer extends React.Component{
 class Controls extends React.Component{
     constructor(props){
       super(props);
-    }
+      this.state={
+        s_time: this.props.s_time,
+        b_time: this.props.b_time,
+        min:this.props.min,
+        max:this.props.max,
+        intervalFn:this.props.intervalFn,
+        intervalTime:this.props.intervalTime,
+        count:this.props.count,
+        play_pause:'Play_Arrow',
+        c:0
+        }
+      }
+     
+      Play_pause=()=>{
+        var mins = document.querySelector('#time-left>h3.display-mins')
+        var secs = document.querySelector('#time-left>h3.display-secs')
+        this.setState({
+          c:this.state.c+=1
+        })
+        console.log(this.state.c)
+        if(this.state.c % 2 !== 0){
+          const Play=()=>{
+           this.setState({
+            s_time: [this.state.s_time[0] = secs.textContent < 1 ? mins.textContent-=this.state.count : mins.textContent, this.state.s_time[1] = secs.textContent < (this.state.min-1) && secs.textContent > 0 ? secs.textContent : secs.textContent = 59],
+            intervalTime: this.state.intervalTime = 1000,
+            intervalFn: this.state.intervalFn = setInterval(() => {
+              // console.log(this.state.c+=1)
+              if(this.state.s_time[0] > 0 && this.state.s_time[1] > 0){
+                 this.setState({
+                  s_time: [this.state.s_time[0] = mins.textContent, this.state.s_time[1] -= this.state.count]})
+              
+              mins.textContent = moreThan10(this.state.s_time[0])
+              secs.textContent = moreThan10(this.state.s_time[1])
+              }
+              else if (this.state.s_time[0] > 0 && this.state.s_time[1] == 0){
+                this.setState({
+                  s_time: [this.state.s_time[0] -= this.state.count, this.state.s_time[1] = 60 ]})
+
+              mins.textContent = moreThan10(this.state.s_time[0])
+              secs.textContent = moreThan10(this.state.s_time[1])
+              }
+
+            }, this.state.intervalTime),
+            play_pause:this.state.play_pause = 'Pause'
+          })
+          }
+          Play()
+        }
+        else{
+          const Pause=()=>{
+            this.setState({
+              // s_time: [this.state.s_time[0] = mins.textContent - this.state.count, this.state.s_time[1] = secs.textContent = 59],
+              // intervalTime: this.state.intervalTime = 500,
+              intervalFn:this.state.intervalFn = clearInterval(this.state.intervalFn),
+              play_pause:this.state.play_pause = 'Play_Arrow'
+            })
+          }
+          Pause()
+        }  
+      }
+   
+      
+      
     render(){
       return(
         
           <div className="controls-actual-container">
             <div className="play-container pause-container">
-            <span id="start_stop" className='material-symbols-outlined play'>Play_Arrow</span>
+            <span onClick={this.Play_pause} id="start_stop" className='material-symbols-outlined play'>{this.state.play_pause}</span>
             </div>
             <div className="reset-container">
             <span className='material-symbols-outlined reset'>Replay</span>
@@ -139,11 +210,15 @@ class Controls extends React.Component{
 //global dependencies
 var sessionMinutes = formatMinutes(25*60)
 var sessionSeconds = (25*60)%60
-var breakMinutes = Math.floor((5 * 60)/60)
-var breakSeconds = (5*60)%60
+var breakMinutes = Math.floor((5*60)/60)
+var breakSeconds = (breakMinutes)%60
 var min = 60
 var max = 60*60
-var interval = null
+var count = 1
+var interval = {
+  fn: null,
+  time:null
+};
 
 const App = () => {
 useEffect(() => {
@@ -157,7 +232,6 @@ useEffect(() => {
         }
     })
 })
-
   return (
     <div id="wrapper">
       <div className="timer-title-container">
@@ -165,14 +239,14 @@ useEffect(() => {
       </div>
       
       <div className="timer-container-parent">
-        <Timer time={[sessionMinutes,sessionSeconds]} min={min} max={max}/>
+        <Timer s_time={[sessionMinutes,sessionSeconds]} b_time={[breakMinutes,breakSeconds]} min={min} max={max}/>
       </div>
       <div className="configuration-container">
         <Break time={[breakMinutes,breakSeconds]} min={min} max={max}/>
-        <Session time={[sessionMinutes,sessionSeconds]} min={min} max={max}/>
+        <Session s_time={[sessionMinutes,sessionSeconds]} b_time={[breakMinutes,breakSeconds]} min={min} max={max}/>
       </div>
       <div className="controls-container">
-        <Controls/>
+        <Controls s_time={[sessionMinutes,sessionSeconds]} b_time={[breakMinutes,breakSeconds]} count ={count} min={min} max={max} intervalFn={interval.fn} intervalTime={interval.time}/>
       </div>
     </div>
   );
--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,6 @@
 //Check if the number is less than 10.
 //If so, The number "0" will be placed infrot of the value.
-const moreThan10 = (num) => {
+export const moreThan10 = (num) => {
     return (num <10) ?`0${num}`: num;
 }
 export function formatMinutes(num){
